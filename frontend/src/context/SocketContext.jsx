65f0b4315f581ae26a795632a0a3c775ab406531
Shaun Lee <xianyang.li@ebram.org>
2025-05-01 21:25:12 +0800

    Socket URL update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -12,10 +12,10 @@ export const SocketContextProvider = ({ children }) => {
 	const [socket, setSocket] = useState(null);
 	const [onlineUsers, setOnlineUsers] = useState([]);
 	const { authUser } = useAuthContext();
-
+	//http://localhost:5003
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5003", {
+			const socket = io("https://realchat-cszt.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
