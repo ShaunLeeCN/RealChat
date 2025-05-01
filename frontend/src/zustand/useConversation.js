5152943b9fabc1d2f78835a3de961904224caec5
Shaun Lee <xianyang.li@ebram.org>
2025-05-01 20:37:55 +0800

    personal changes

--- frontend/src/zustand/useConversation.js
+++ frontend/src/zustand/useConversation.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 const useConversation = create((set) => ({
-	selectedConversation: null,
+	selectedConversation: null,//存储&更新当前选中的会话对象
 	setSelectedConversation: (selectedConversation) => set({ selectedConversation }),
-	messages: [],
+	messages: [],//存储&更新当前会话的消息列表
 	setMessages: (messages) => set({ messages }),
 }));
 
